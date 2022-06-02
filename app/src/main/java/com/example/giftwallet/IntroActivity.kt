7ea202c768947ede127846997a38cb3bff29abf7
Park <deflever@gmail.com>
2022-06-02 14:29:09 +0900

    20220602 intro, icon 변경완료

--- app/src/main/java/com/example/giftwallet/IntroActivity.kt
+++ app/src/main/java/com/example/giftwallet/IntroActivity.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
+import com.bumptech.glide.Glide
+import com.example.giftwallet.databinding.IntroLayoutBinding
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var binding: IntroLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.intro_layout)
+        binding = IntroLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+         Glide.with(this).load(R.raw.intro_cat).into(binding.ivIntroCat)
+
         var handler = Handler()
         handler.postDelayed({
             var intent = Intent(this, MainActivity::class.java)
             startActivity (intent)
-        }, 2000)
+        }, 1000)
     }
 
     override fun onPause() {
